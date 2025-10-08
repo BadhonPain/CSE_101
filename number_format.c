8fadf3a8ab59fa9903cf24e5dd829b70407e6157
Test_Case <badhonpain48@gmail.com>
2025-10-08 17:08:38 +0600

    06

---
+++ number_format.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// we can input hexadecimal and octal also in this code
+int main()
+{
+    int x;
+    scanf("%i", &x);
+    printf("ASCII: %i Character: %c\n", x, x);
+    return 0;
+}
